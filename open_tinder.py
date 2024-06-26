416d617c2df8e9e1a5ef344cfbf48dcd91552fd5
kiringodhwani <kiringodhwani@gmail.com>
2024-06-25 23:46:32 -0400

    Final Changes

--- open_tinder.py
+++ open_tinder.py
@@ -5,8 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-# Kirin 1
-
 from config import email, password
 
 class OpenTinder:
